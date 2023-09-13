610c42056351eaacf80cabc9639292723b30a81b
yangyaojia <y11011@yeah.net>
2023-09-13 14:32:37 +0800

    demo

---
+++ com/jiajia/EureApplication.java
@@ -0,0 +1,10 @@
+package com.jiajia;
+
+import javax.annotation.Resource;
+
+@Resource
+public class EureApplication {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
